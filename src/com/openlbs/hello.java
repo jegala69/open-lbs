8885014cc8661bf3eb33d0663316fad07dc3ab55
Jesus Garcia Lazaro <jesgarcialazaro@gmail.com>
2012-12-04 10:04:09 +0100

    primer java

---
+++ src/com/openlbs/hello.java
@@ -0,0 +1,13 @@
+package com.openlbs;
+
+public class hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello!");
+	}
+
+}
